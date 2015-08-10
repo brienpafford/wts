d80eb0c00a6e7cacb0f97d69707347d374623b88
Brien Pafford <bcpaff@gmail.com>
2015-08-10 10:44:45 -0500

    boilerplate from Yo

---
+++ client/lib/app.ng.js
@@ -0,0 +1,15 @@
+angular.module('wtsApp', [
+  'angular-meteor',
+  'ui.router',
+  'angularUtils.directives.dirPagination'
+]);
+
+onReady = function() {
+  angular.bootstrap(document, ['wtsApp']);
+};
+  
+if(Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
